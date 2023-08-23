51f591cfdbd46b8a69ea0ad9691d4f32fbaca88b
T_T <ice257@yandex.ru>
2023-08-23 07:32:25 +0300

    Initial commit

---
+++ src/main/java/com/example/emu3/Emu3Application.java
@@ -0,0 +1,13 @@
+package com.example.emu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emu3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Emu3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emu3/Application.java
@@ -0,0 +1,13 @@
+package com.example.emu3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emu3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
